668091ae06ac4fc3fb7f0b485046412b9c666c49
codeskyblue <codeskyblue@gmail.com>
2018-11-06 16:35:31 +0800

    remove wrong bat files

--- build-run.sh
+++ build-run.sh
@@ -3,13 +3,19 @@
 
 set -ex
 
-ADB="adb.exe"
+ADB=${ADB:-"adb.exe"}
 DEST="/data/local/tmp/atx-agent"
 
+echo "Build binary for arm ..."
 GOOS=linux GOARCH=arm go build
+
+# go generate
+# GOOS=linux GOARCH=arm go build -tags vfs
+
 $ADB push atx-agent $DEST
 $ADB shell chmod 755 $DEST
 $ADB shell $DEST server --stop
 $ADB shell $DEST server -d
+
 $ADB forward tcp:7912 tcp:7912
 curl localhost:7912/wlan/ip
